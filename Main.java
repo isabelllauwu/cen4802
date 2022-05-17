d784279187fad91eba73104d0c2f750036626b99
isabelllauwu <83238391+isabelllauwu@users.noreply.github.com>
2022-05-17 10:08:38 -0400

    Revert "Chnages"

--- Main.java
+++ Main.java
@@ -1,25 +1,12 @@
 import java.util.*;
-/**
- * A recursive static method that returns nth term in Fibonacci sequence
- * 
- * @author Isabella Correa Castano
- */
+
 public class Main
 {
-    
-    /** 
-     * @param n
-     * @return int
-     */
     public static int fibonacci(int n) {
         if (n<=1)
         return n;
         return fibonacci(n-1) + fibonacci(n-2);
     }
-    
-    /** 
-     * @param args[]
-     */
     public static void main(String args[])
     
     {
